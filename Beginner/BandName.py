776257ac6660b7e9eaab85e0dbd73877ddf17075
MOIN UL HAQ <139452235+moinulhaq52@users.noreply.github.com>
2026-06-02 15:19:28 +0500

    Delete Beginner directory

--- Beginner/BandName.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-city = input("Tell the name of a city where you grew up:\n")
-pet = input("Tell the name of your pet:\n")
-print("The band name is:" + " " + city + " " + pet)
